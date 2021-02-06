5c5cd91b1ad11318673dfa3edb1be55e3aa59aac
Magdalena Biernacka <magdalenabiernacka02@gmail.com>
2021-02-06 18:42:39 +0100

    Hello World

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {         // [2]
+        System.out.println("Hello World!");
+        System.out.println("Magdalena");
+        System.out.println(28);
+        System.out.println('B');
+        }
+    }
