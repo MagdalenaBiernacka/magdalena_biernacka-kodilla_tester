f5e7e13c51e7f579a515681ab0e7ddf481eb18a6
Magdalena Biernacka <magdalenabiernacka02@gmail.com>
2021-02-06 19:26:41 +0100

    MainExtended

---
+++ src/main/java/MainExtended.java
@@ -0,0 +1,16 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String myName = "Magdalena";
+        int sampleNumber = 13;
+        String myCountry = "Polska";
+        boolean isGoodDay = true;
+        char favoriteLetter = 'A';
+        String myDog = "Akiko";
+        System.out.println(myName);
+        System.out.println(sampleNumber);
+        System.out.println(myCountry);
+        System.out.println(isGoodDay);
+        System.out.println(favoriteLetter);
+        System.out.println(myDog);
+    }
+}
