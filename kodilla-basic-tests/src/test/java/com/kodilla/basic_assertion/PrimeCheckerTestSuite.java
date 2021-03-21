333e638df3aefa57badb5d0a8634b485f9e7b0ff
Magdalena Biernacka <magdalenabiernacka02@gmail.com>
2021-03-21 16:45:08 +0100

    Grades

--- kodilla-basic-tests/src/test/java/com/kodilla/basic_assertion/PrimeCheckerTestSuite.java
+++ kodilla-basic-tests/src/test/java/com/kodilla/basic_assertion/PrimeCheckerTestSuite.java
@@ -13,45 +13,54 @@ public class PrimeCheckerTestSuite {
 //    przekazujemy cyfrę 0, oczekujemy false,
 //    przekazujemy cyfrę -6, oczekujemy false.
 
+    private PrimeChecker checker = new PrimeChecker();
+    private static int count = 0;
+
     @Test
     public void shouldReturnFalseWhenPassingNotPrimeNumber() {
-        PrimeChecker checker = new PrimeChecker();
         boolean result = checker.isPrime(14);
+        count++;
+        System.out.println("Test number " + count);
         assertFalse(result);
     }
 
     @Test
     public void shouldReturnTrueWhenPassingPrimeNumber() {
-        PrimeChecker checker = new PrimeChecker();
         boolean result = checker.isPrime(13);
+        count++;
+        System.out.println("Test number " + count);
         assertTrue(result);
     }
 
     @Test
     public void shouldReturnTrueWhenPassingTwo() {
-        PrimeChecker checker = new PrimeChecker();
         boolean result = checker.isPrime(2);
+        count++;
+        System.out.println("Test number " + count);
         assertTrue(result);
     }
 
     @Test
     public void shouldReturnFalseWhenPassingOne() {
-        PrimeChecker checker = new PrimeChecker();
         boolean result = checker.isPrime(1);
+        count++;
+        System.out.println("Test number " + count);
         assertFalse(result);
     }
 
     @Test
     public void shouldReturnFalseWhenPassingZero() {
-        PrimeChecker checker = new PrimeChecker();
         boolean result = checker.isPrime(0);
+        count++;
+        System.out.println("Test number " + count);
         assertFalse(result);
     }
 
     @Test
     public void shouldReturnFalseWhenPassingNegativeNumber() {
-        PrimeChecker checker = new PrimeChecker();
         boolean result = checker.isPrime(-6);
+        count++;
+        System.out.println("Test number " + count);
         assertFalse(result);
     }
 }
