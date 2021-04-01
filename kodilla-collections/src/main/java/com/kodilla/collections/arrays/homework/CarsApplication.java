e43721e2a271918a08ee4ae35ff5377320fab021
Magdalena Biernacka <magdalenabiernacka02@gmail.com>
2021-04-01 19:14:31 +0200

    Random Cars v.2

--- kodilla-collections/src/main/java/com/kodilla/collections/arrays/homework/CarsApplication.java
+++ kodilla-collections/src/main/java/com/kodilla/collections/arrays/homework/CarsApplication.java
@@ -8,31 +8,42 @@ import com.kodilla.collections.interfaces.homework.Opel;
 import java.util.Random;
 
 public class CarsApplication {
+    static Car[] cars;
+    static Random random;
 
     public static void main(String[] args) {
-
-        Random random = new Random();
-        Car[] cars = new Car[random.nextInt(20)];
-
-        System.out.println("Wylosowano: " + cars.length);
-
-        for (int i = 0; i < cars.length; i++)
-            cars[i] = drawCar();
-
-        for (Car car: cars)
-            CarUtils.describeCar(car);
+        random = new Random();
+        generateCars();
+        displayCarData(cars);
     }
 
     public static Car drawCar() {
-        Random random = new Random();
         int drawnCarKind = random.nextInt(3);
-        int a = random.nextInt() * 200 + 1;
-        int b = random.nextInt() * 50 + 1;
+        System.out.println("Wylosowano: " + cars.length);
+        int a = random.nextInt(200) + 1;
+        int b = random.nextInt(50) + 1;
         if (drawnCarKind == 0)
             return new Opel(a, b);
         else if (drawnCarKind == 1)
             return new Ford(a, b);
         else
             return new Mercedes(a, b);
+    }
+
+    public static void displayCarData(Car[] cars) {
+        for (Car car: cars) {
+            car = drawCar();
+            car.increaseSpeed();
+            car.decreaseSpeed();
+            CarUtils.describeCar(car);
+        }
+    }
+
+    public static void generateCars() {
+        cars = new Car[random.nextInt(20)];
+        if (cars.length == 0) {
+            System.out.println( "Wylosowano 0, ponowne losowanie");
+            cars = new Car[random.nextInt(20)];
+        }
     }
 }
