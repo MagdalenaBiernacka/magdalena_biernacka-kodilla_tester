a14b8695040cc06e04e05aab89a5269467184fe5
Magdalena Biernacka <magdalenabiernacka02@gmail.com>
2021-04-27 23:01:14 +0200

    Average Number of Posts

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/homework/ForumStats.java
@@ -0,0 +1,26 @@
+package com.kodilla.stream.homework;
+
+import com.kodilla.stream.UsersRepository;
+
+public class ForumStats {
+    public static void main(String[] args) {
+        double avgForMoreThen40 = UsersRepository.getUsersList()
+
+                .stream()
+                .filter(user -> user.getAge()>= 40)
+                .map(user -> user.getNumberOfPost())
+                .mapToInt(value -> value)
+                .average()
+                .getAsDouble();
+        System.out.println("średnia liczba postów dla użytkowników powyżej 40-roku życia: " + avgForMoreThen40);
+
+        double avgForLessThen40 = UsersRepository.getUsersList()
+                .stream()
+                .filter(user -> user.getAge()< 40)
+                .map(user -> user.getNumberOfPost())
+                .mapToInt(value -> value)
+                .average()
+                .getAsDouble();
+        System.out.println("średnia liczba postów dla użytkowników poniżej 40-roku życia: " + avgForLessThen40);
+    }
+}
