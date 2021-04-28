aa3fd6c8f0ad122fac23cb488f62cf4960bbe3fa
Magdalena Biernacka <magdalenabiernacka02@gmail.com>
2021-04-28 21:05:39 +0200

    Warehouse

---
+++ kodilla-stream/src/main/java/com/kodilla/exception/homework/WarehouseApp.java
@@ -0,0 +1,25 @@
+package com.kodilla.exception.homework;
+
+public class WarehouseApp {
+    public static void main(String[] args) {
+        Warehouse warehouse = new Warehouse();
+        warehouse.addOrder(new Order("1"));
+        warehouse.addOrder(new Order("2"));
+        warehouse.addOrder(new Order("3"));
+        warehouse.addOrder(new Order("4"));
+        warehouse.addOrder(new Order("5"));
+        warehouse.addOrder(new Order("6"));
+        warehouse.addOrder(new Order("7"));
+        warehouse.addOrder(new Order("8"));
+        warehouse.addOrder(new Order("9"));
+
+        try {
+            Order order = warehouse.getOrder("1");
+            System.out.println("Order: " + order);
+        } catch (OrderDoesntExistException exception) {
+            System.out.println("Sorry, this order can not be found.");
+        } finally {
+            System.out.println("OK");
+        }
+    }
+}
